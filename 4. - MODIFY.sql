a7adae43625d3ebc8551d11141f7b06fc0ec0804
Dario <dariomrkonjic1@gmail.com>
2022-12-12 20:46:04 +0100

    Done.

--- 4. - MODIFY.sql
+++ 4. - MODIFY.sql
@@ -9,46 +9,6 @@ DELETE FROM Members
 WHERE DATE_PART('year', NOW()) - DATE_PART('year', Members.DateOfBirth) > 25
 
 -- Kick all interns whose grade average for a given field is less than 2.4:
---UPDATE InternsFieldsInternships
---SET Status = 'Kicked'
---WHERE InternId IN (
---	SELECT ifi.InternId FROM InternsFieldsInternships ifi
---	JOIN Interns i ON ifi.InternId = i.Id
---	JOIN InternsAssignments iass ON iass.InternId = i.Id
---);
-		 
---UPDATE InternsFieldsInternships
---SET Status = 'Kicked'
---WHERE (
---	SELECT AVG(iass.Grade) FROM InternsFieldsInternships ifi
---	JOIN Interns i ON ifi.InternId = i.Id
---	JOIN InternsAssignments iass ON iass.InternId = i.Id
---		 ) < 2.4;
-
--- Testing: For each intern get the average grade for each applied field.
---SELECT * FROM InternsFieldsInternships;
-
---SELECT SUB.Avg, SUB.FieldId, i.Id FROM Fields
---JOIN (
---	SELECT AVG(iass.Grade), ifi.FieldId, i.Id FROM InternsAssignments iass
---	JOIN Interns i ON iass.InternId = i.id
---	JOIN InternsFieldsInternships ifi ON ifi.InternId = i.Id
---	JOIN Fields f ON ifi.FieldId = f.Id
---	GROUP BY ifi.FieldId, i.Id
---) as SUB ON Fields.id = SUB.FieldId
---JOIN Interns i ON SUB.Id = i.Id;
-
---SELECT SUB.Avg, SUB.FieldId, i.Id FROM Fields
---JOIN (
---	SELECT AVG(iass.Grade), ifi.FieldId, i.Id FROM InternsAssignments iass
---	JOIN Interns i ON iass.InternId = i.id
---	JOIN InternsFieldsInternships ifi ON ifi.InternId = i.Id
---	JOIN Fields f ON ifi.FieldId = f.Id
---	GROUP BY ifi.FieldId, i.Id
---	HAVING AVG(iass.Grade) > 3
---) as SUB ON Fields.id = SUB.FieldId
---JOIN Interns i ON SUB.Id = i.Id;
-
 UPDATE internsfieldsinternships
 SET Status = 'Kicked'
 FROM (
@@ -60,6 +20,3 @@ FROM (
 	HAVING AVG(iass.Grade) < 2.4
 ) as SUB
 WHERE SUB.FieldId = internsfieldsinternships.FieldId AND SUB.Id = internsfieldsinternships.InternId;
-
-
-
